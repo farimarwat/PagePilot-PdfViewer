2b40d9d663a798a7b4adcc0b4a0b3f3b82f757ce
Farman Ullah Khan <farimarwat@gmail.com>
2023-10-27 15:33:59 +0500

    initial release

---
+++ app/src/main/java/com/farimarwat/pagepilotexample/PdfItem.kt
@@ -0,0 +1,13 @@
+package com.farimarwat.pagepilotexample
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview
+fun PdfItemPreview(){
+    PdfItem()
+}
+@Composable
+fun PdfItem() {
+}
